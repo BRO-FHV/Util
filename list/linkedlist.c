e93d8b3b9d3f37304342ad1061d79af9b9ec6100
Johannes Wachter <johannes.wachter@students.fhv.at>
2014-03-17 21:39:49 +0100

    implemented linked list append front get front

---
+++ list/linkedlist.c
@@ -0,0 +1,61 @@
+/*
+ * Util: linkedlist.c
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: Mar 17, 2014
+ * Description: 
+ * TODO
+ */
+
+#include <stdlib.h>
+#include "linkedlist.h"
+
+ll_t* LinkedListCreate() {
+	ll_t* ll = (ll_t*) malloc(sizeof(ll_t));
+	ll->back = NULL;
+	ll->front = NULL;
+
+	return ll;
+}
+
+void LinkedListAppendFront(ll_t* list, void* data) {
+	llnode_t* node = (llnode_t*) malloc(sizeof(llnode_t));
+	node->data = data;
+	if (list->front == list->back) {
+		if (list->front == NULL) {
+			list->front = node;
+			list->back = node;
+		} else {
+			node->parent = list->back;
+			list->back->parent = node;
+			list->front = node;
+		}
+	} else {
+		node->next = list->front;
+		list->front->parent = node;
+		list->front = node;
+	}
+}
+void LinkedListAppendBack(ll_t* list, void* data) {
+}
+
+void* LinkedListGetFront(ll_t* list) {
+	if (list->front == list->back) {
+		if (list->front == NULL) {
+			return NULL;
+		} else {
+			llnode_t* tmp = list->front;
+			list->front = NULL;
+			list->back = NULL;
+			return tmp->data;
+		}
+	} else {
+		llnode_t* tmp = list->front;
+		list->front = tmp->next;
+		list->front->parent = NULL;
+		return tmp->data;
+	}
+}
+void* LinkedListGetBack(ll_t* list) {
+	return NULL;
+}
