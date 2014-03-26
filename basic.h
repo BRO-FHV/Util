147e63299c07dc8ab032f34dd52ff78ab1bd8cf3
sla89 <skullsplitter@die-optimisten.net>
2014-03-26 22:53:40 +0100

    code clean up, fixed warnings, added some comments

---
+++ basic.h
@@ -0,0 +1,49 @@
+/*
+ * Hal: basic.h
+ * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
+ *
+ * Created on: Mar 14, 2014
+ * Description: 
+ * Define basic register operations and standard type bool, ture/false
+ * and null
+ *
+ */
+
+
+
+#ifndef BASIC_H_
+#define BASIC_H_
+
+#include <inttypes.h>
+
+#ifndef NULL
+#define NULL ((void*) 0)
+#endif
+
+/** Define a boolean type, and values for true and false. */
+
+typedef unsigned char tBoolean;
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef false
+#define false 0
+#endif
+
+#define TRUE    1
+#define FALSE   0
+
+
+#define wait(x)	while(x);
+#define reg32r(b, r) (*(volatile uint32_t *)((b)+(r)))
+#define reg32w(b, r, v) (*((volatile uint32_t *)((b)+(r))) = (v))
+#define reg32m(b, r, v) (*((volatile uint32_t *)((b)+(r))) |= (v))
+#define reg32a(b, r, v) (*((volatile uint32_t *)((b)+(r))) &= (v))
+#define reg32an(b, r, v) (*((volatile uint32_t *)((b)+(r))) &= ~(v))
+
+#define reg32wor(b, r, v) (*((volatile uint32_t *)((b)+(r))) |= (v))
+#define reg32wxor(b, r, v) (*((volatile uint32_t *)((b)+(r))) ^= (v))
+
+#endif /* BASIC_H_ */
